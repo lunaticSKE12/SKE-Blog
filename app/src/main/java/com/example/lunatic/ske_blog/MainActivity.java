c4805100d2b49027a767b2c0db9640c38f870433
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-23 00:22:31 +0700

    setup firebase and add the add icon

---
+++ app/src/main/java/com/example/lunatic/ske_blog/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lunatic.ske_blog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        
+        return super.onCreateOptionsMenu(menu);
+    }
+}
