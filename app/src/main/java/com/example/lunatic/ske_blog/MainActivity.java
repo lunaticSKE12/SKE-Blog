6dee334c15c01cf4c8559c910cdcda7499c80c96
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-23 01:15:02 +0700

    update

--- app/src/main/java/com/example/lunatic/ske_blog/MainActivity.java
+++ app/src/main/java/com/example/lunatic/ske_blog/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        
+
         return super.onCreateOptionsMenu(menu);
     }
 }
