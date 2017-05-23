6e93ddacf667632c2b4d009c82da63e1226281e4
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-23 15:27:23 +0700

    upload image to firebase and edit description field size
    generate name of image for unique name and show alert

--- app/src/main/java/com/example/lunatic/ske_blog/PostActivity.java
+++ app/src/main/java/com/example/lunatic/ske_blog/PostActivity.java
@@ -1,58 +1,159 @@
 package com.example.lunatic.ske_blog;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Random;
 
 /**
  * PostActivity post page to add image, title, and description.
+ *
  * @author napong dungduangsasitorn
  */
 public class PostActivity extends AppCompatActivity {
 
-    private ImageButton mSelectImage;
+	private ImageButton mSelectImage;
+	private EditText mPostTitle;
+	private EditText mPosrDesc;
+	private Button mSubmitBtn;
+	private Uri mImageUri = null;
+	private StorageReference mStorage;
+	private ProgressDialog mProgress;
+	private String random;
 
-    private static final int GALLERY_REQUEST = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
-
-        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
-
-        mSelectImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, GALLERY_REQUEST);
-            }
-        });
-    }
+	private static final int GALLERY_REQUEST = 1;
 
 
-    /**
-     * onActivityResult 
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+	/**
+	 * create views, bind data to lists
+	 *
+	 * @param savedInstanceState
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_post);
 
-        /**
-         * request image from gallery and get image.
-         */
-        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
+		mStorage = FirebaseStorage.getInstance().getReference();
+		mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+		mPostTitle = (EditText) findViewById(R.id.titleField);
+		mPosrDesc = (EditText) findViewById(R.id.titleField);
+		mSubmitBtn = (Button) findViewById(R.id.summitBtn);
+		mProgress = new ProgressDialog(this);
 
-            mSelectImage.setImageURI(imageUri);
-        }
-    }
+
+		mSelectImage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+				galleryIntent.setType("image/*");
+				startActivityForResult(galleryIntent, GALLERY_REQUEST);
+			}
+		});
+
+		mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				startPosting();
+			}
+		});
+
+	}
+
+	/**
+	 * startPosting post to blog
+	 */
+	private void startPosting() {
+
+		mProgress.setMessage("Posting to Blog");
+		mProgress.show();
+
+		String title_val = mPostTitle.getText().toString().trim();
+		String desc_val = mPosrDesc.getText().toString().trim();
+
+		if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null) {
+
+			// get file path and random image name.
+			StorageReference filepath = mStorage.child("Blog_Images").child(random());
+
+			filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+				@Override
+				public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+					Uri downloadUrl = taskSnapshot.getDownloadUrl();
+					mProgress.dismiss();
+
+				}
+			});
+
+		} else {
+			//if image or title field or description empty will show alert.
+			new AlertDialog.Builder(PostActivity.this)
+					.setTitle("Alert")
+					.setMessage("Title and Description and Image can't empty")
+					.setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					mProgress.dismiss();
+				}
+			}).show();
+		}
+
+	}
+
+	/**
+	 *
+	 * @return random string
+	 */
+	public static String random() {
+		Random generator = new Random();
+		StringBuilder randomStringBuilder = new StringBuilder();
+		int randomLength = generator.nextInt(20);
+		char tempChar;
+		for (int i = 0; i < randomLength; i++) {
+			tempChar = (char) (generator.nextInt(96) + 32);
+			randomStringBuilder.append(tempChar);
+		}
+		return randomStringBuilder.toString();
+	}
+
+	/**
+	 * onActivityResult start an activity that lets the user pick a person in a list of contacts
+	 *
+	 * @param requestCode
+	 * @param resultCode
+	 * @param data
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		/**
+		 * request image from gallery and get image.
+		 */
+		if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+			mImageUri = data.getData();
+
+			mSelectImage.setImageURI(mImageUri);
+		}
+	}
+
+
 }
