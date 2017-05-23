c2173ae8a237a8ba67b8e74dcbb4e3d0f21622db
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-23 14:00:12 +0700

    get image from gallery for post

--- app/src/main/java/com/example/lunatic/ske_blog/PostActivity.java
+++ app/src/main/java/com/example/lunatic/ske_blog/PostActivity.java
@@ -1,13 +1,58 @@
 package com.example.lunatic.ske_blog;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
+/**
+ * PostActivity post page to add image, title, and description.
+ * @author napong dungduangsasitorn
+ */
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+    }
+
+
+    /**
+     * onActivityResult 
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        /**
+         * request image from gallery and get image.
+         */
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+
+            mSelectImage.setImageURI(imageUri);
+        }
     }
 }
