9dae61314c0426a88578e066872c7da18a157ed9
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-23 13:26:11 +0700

    set icon post and new post activity

--- app/src/main/java/com/example/lunatic/ske_blog/MainActivity.java
+++ app/src/main/java/com/example/lunatic/ske_blog/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.lunatic.ske_blog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
+/**
+ * MainActivity is first page in android app
+ *
+ * @author napong dungduangsasitorn
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * onCreate is create page view by activity_main
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
 
-
+    /**
+     * onCreateOptionsMenu set menu icon and action.
+     * @param menu is menu from main_menu.
+     * @return new menu.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * onOptionsItemSelected set action on add icon
+     * to start new page(new Activity).
+     * @param item is MenuItem.
+     * @return item.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_add){
+            startActivity(new Intent(MainActivity.this, PostActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
